2cd474217e59c2c9a86ddc7b54a757b49cd3695f
Kaylaine Vieira <kaylainevieira3@gmail.com>
2022-03-18 20:23:43 -0300

    Início da criação dos cadastros de mercadoria e colaborador

---
+++ src/br/com/senai/supermercadosenai/entity/Colaborador.java
@@ -0,0 +1,24 @@
+package br.com.senai.supermercadosenai.entity;
+
+public class Colaborador {
+
+	private String nome;
+	
+	private String cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
